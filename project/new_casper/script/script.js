a8277d697406479aac668d9fcaea76546dbe2229
Dahee <57856201+Dev-hee@users.noreply.github.com>
2024-06-03 18:13:21 +0900

    240603

---
+++ project/new_casper/script/script.js
@@ -0,0 +1,13 @@
+//변수
+const sub = document.querySelectorAll('.sub')
+const sub_bg = document.querySelector('.sub_bg')
+const nav = document.querySelector('nav')
+const nav_clone = nav.cloneNode(true)//nav 복제 변수
+const m_nav = document.querySelector('.m_nav') // 복제 붙여놓기 부모
+console.log(sub, sub_bg, nav, nav_clone, m_nav)
+// 모든 서브 숨기기
+sub_bg.style.display='none'
+for(let i of sub){i.style.display = 'none'}
+
+//m_nav 모바일 전용 메뉴의 기존 내비게이션 태그 마지막 자식 봍붙
+m_nav.appendChild(nav_clone)
