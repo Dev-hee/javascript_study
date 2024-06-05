bb3d0fd73c068cde890cd03d1c1613e1df97c1c2
Dahee <57856201+Dev-hee@users.noreply.github.com>
2024-06-05 18:04:52 +0900

    240605- 캐스퍼_제이쿼리 ver

--- project/new_casper/script/script.js
+++ project/new_casper/script/script.js
@@ -5,9 +5,34 @@ const nav = document.querySelector('nav')
 const nav_clone = nav.cloneNode(true)//nav 복제 변수
 const m_nav = document.querySelector('.m_nav') // 복제 붙여놓기 부모
 console.log(sub, sub_bg, nav, nav_clone, m_nav)
+
 // 모든 서브 숨기기
-sub_bg.style.display='none'
-for(let i of sub){i.style.display = 'none'}
+//sub_bg.style.display='none'
+//for(let i of sub){i.style.display = 'none'}
 
 //m_nav 모바일 전용 메뉴의 기존 내비게이션 태그 마지막 자식 봍붙
 m_nav.appendChild(nav_clone)
+
+//메뉴에 마우스 올리면 서브 메뉴 나타나기(높이 길어지는 효과)
+// height + transition + overflow
+//1. 숨기는 처리를 display 가 아닌 height 값으로 재설정 = 0
+sub_bg.style.height = '0';
+sub_bg.style.transition = 'height 0.3s';
+for(let i of sub){
+    i.style.height = '0'; 
+    i.style.overflow = 'hidden'; 
+    i.style.transition = 'height 0.3s'; 
+}
+//2. 메뉴 마우스 올리면 서브, 서브배경 보이기
+nav.addEventListener('mouseover',()=>{
+    sub_bg.style.height = '220px'; 
+for(let i of sub){
+    i.style.height = '220px'; 
+}
+})
+nav.addEventListener('mouseout',()=>{
+    sub_bg.style.height = '0'; 
+for(let i of sub){
+    i.style.height = '0'; 
+}
+})
